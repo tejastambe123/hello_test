cbfa9749ecdfbee8a2667649f5c2241c83a40744
tejastambe123 <tejastambe0808@gmail.com>
2026-06-12 17:18:03 +0530

    updated file

--- firstCode.c
+++ firstCode.c
@@ -1,20 +1,3 @@
-#include <stdio.h>
+nclude <stdio.h>
 
-int main() {
-    int num1, num2, sum;
 
-    // Ask the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-
-    // Calculate the sum
-    sum = num1 + num2;
-
-    // Display the result
-    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
-
-    return 0;
-}
